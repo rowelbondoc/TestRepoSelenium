fb3d8c4487df94b19754dbf5a9a7e7d95bf4877c
rowelbondoc <rowelbondoc06@gmail.com>
2021-10-18 16:06:47 +0800

    Initial commit until combo controls
    Initial commit until combo controls

---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests: DriverHelper
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almonds");          
+
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
